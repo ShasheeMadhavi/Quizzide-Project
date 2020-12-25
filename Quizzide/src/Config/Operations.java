8d79baa9faf270cc4deedf6394700e32eaf6ddd1
Shashee Madhavi <75610275+ShasheeMadhavi@users.noreply.github.com>
2020-12-26 02:29:48 +0530

    quiz creation added!

--- Quizzide/src/Config/Operations.java
+++ Quizzide/src/Config/Operations.java
@@ -8,6 +8,8 @@ package Config;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
@@ -81,6 +83,71 @@ public class Operations {
                 JOptionPane.showMessageDialog(frame, "Data has been saved!");
             } catch (Exception exception) {
                 JOptionPane.showMessageDialog(frame, "Data Error: " + exception.getMessage());
+            }
+            myConn.close();
+            
+        } catch(Exception exception) {
+            JOptionPane.showMessageDialog(frame, "Error: " + exception.getMessage());
+        }
+    }
+    
+    public static void course2Combo(JComboBox coursesBox, JFrame frame) {
+        try {
+            Connection myConn = MySQLConnection.getConnection();
+            String sqlQuery = "SELECT course_id FROM tbl_courses";
+            
+            PreparedStatement preparedStatement = myConn.prepareStatement(sqlQuery);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            DefaultComboBoxModel comboModel = (DefaultComboBoxModel)coursesBox.getModel();
+            comboModel.removeAllElements();
+            while (resultSet.next()) {
+                comboModel.addElement(resultSet.getString("course_id"));
+            }
+            myConn.close();
+            
+        } catch (Exception exception) {
+            JOptionPane.showMessageDialog(frame, "Error: " + exception.getMessage());
+        }
+    }
+    
+    public static void saveQuiz(String quizTitle, String courseID, JFrame frame) {
+        try {
+            Connection myConn = MySQLConnection.getConnection();
+            String sqlQuery = "INSERT INTO tbl_quiz (quiz_name, course_id) VALUES (?, ?)";
+            
+            PreparedStatement preparedStatement = myConn.prepareStatement(sqlQuery);
+            preparedStatement.setString(1, quizTitle);
+            preparedStatement.setString(2, courseID);
+            
+            try {
+                preparedStatement.execute();
+                JOptionPane.showMessageDialog(frame, "Data has been saved!");
+            } catch (Exception exception) {
+                JOptionPane.showMessageDialog(frame, "Data Error: " + exception.getMessage());
+            }
+            myConn.close();
+            
+        } catch(Exception exception) {
+            JOptionPane.showMessageDialog(frame, "Error: " + exception.getMessage());
+        }
+    }
+    
+    public static void viewQuiz(JTable quizTable, JFrame frame, String sqlQuery) {
+        try {
+            Connection myConn = MySQLConnection.getConnection();
+            PreparedStatement preparedStatement = myConn.prepareStatement(sqlQuery);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            DefaultTableModel tableModel = (DefaultTableModel)quizTable.getModel();
+            tableModel.setRowCount(0);
+            while (resultSet.next()) {
+                Object coursesObject[] = {
+                    resultSet.getString("quiz_id"),
+                    resultSet.getString("quiz_name"),
+                    resultSet.getString("course_id")
+                };
+                tableModel.addRow(coursesObject);
             }
             myConn.close();
             
